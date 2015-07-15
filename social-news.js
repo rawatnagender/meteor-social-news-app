4bc5c83cb056c813c7c94727852c22a8a25d3114
Indresh <indresh@indresh.(none)>
2015-07-15 19:10:28 +0530

    file upload with collection:fs

--- social-news.js
+++ social-news.js
@@ -1,17 +1,20 @@
 News = new Mongo.Collection("news");
 
-var Images = new FS.Collection("images", {
-  stores: [new FS.Store.GridFS("images")]
+var
+
+Images = new FS.Collection("Images", {
+  stores: [new FS.Store.FileSystem("Images", {path: "~/uploads"})]
 });
 
 Meteor.methods({
     addNews: function (title,url) {
-        News.insert({
-            title: title,
-            url:url,
-            urlTitle:title.replace(/\s/g,'-'),
-            dateAdded:new Date()
-        });
+      alert("inside add_news");
+        // News.insert({
+        //     title: title,
+        //     url:url,
+        //     urlTitle:title.replace(/\s/g,'-'),
+        //     dateAdded:new Date()
+        // });
     }
 });
 
@@ -31,35 +34,62 @@ if (Meteor.isClient) {
     });
 
     Template.addNews.events({
-       'submit .addNewsForm':function(e){
-
+       'submit .addNewsForm':function(e, template){
            var title= e.target.title.value;
 
            var url= e.target.url.value;
+           alert("hello1")
 
-           var files = event.target.files;
+           var news_lat = News.insert({
+            title: title,
+            url:url,
+            urlTitle:title.replace(/\s/g,'-'),
+            dateAdded:new Date()
+           }, function(err, result){
 
+            if(result){
+              alert(result);
+              alert("news successfully added");
 
-           if(!title || !url){
-               return false;
            }
+         });
+           alert("hello2")
 
+            var files = template.find('input:file');
+            alert(files);
             for (var i = 0, ln = files.length; i < ln; i++) {
+            alert("inside for loop");
             Images.insert(files[i], function (err, fileObj) {
+              alert("image inserted successfully")
+              alert(fileObj);
               // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
             });
-            }
-           Meteor.call('addNews',title,url);
+          }
+          alert("after for loop");
+
+          // FS.Utility.eachFile(event, function(file) {
+          //     alert("in utility");
+          //     alert(file);
+          //     Images.insert(file, function (err, fileObj) {
+          //       if (err){
+          //         // handle error
+          //       } else {
+          //           // handle success depending what you need to do
+          //         var userId = Meteor.userId();
+          //         var imagesURL = {
+          //           "profile.image": "/cfs/files/images/" + fileObj._id
+          //         };
+          //         News.update({_id:news_lat}, {$set: imagesURL});
+          //       }
+          //     });
+          //   });
+          alert("after utility");
+           // Meteor.call('addNews',title,url);
 
            Router.go('news.all');
 
            return false;
-       },
-
-       'change .myFileInput': function(event, template) {
-
-
-        }
+       }
     });
 }
 
