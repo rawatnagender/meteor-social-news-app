13b9c76b0da1ef62ab3b131718e2a84191051052
Indresh <indresh@indresh.(none)>
2015-07-16 19:37:20 +0530

    done image processing

--- social-news.js
+++ social-news.js
@@ -1,23 +1,37 @@
 News = new Mongo.Collection("news");
 
-var
+// Meteor.startup(function(){
+//   process.env.MAIL_URL = smtp:nagi.rawat4@gmail.com:nrnrnene@smtp.gmail.com:587/
+// });
+
+
+// Images = new FS.Collection("Images", {
+//   stores: [new FS.Store.FileSystem("Images", {path: "~/uploads"})]
+// });
+// Email.send({
+//   from: "nagi.rawat4@gmail.com",
+//   to: "nagi.rawat3@gmail.com",
+//   subject: "Meteor Can Send Emails via Gmail",
+//   text: "Its pretty easy to send emails via gmail."
+// });
 
 Images = new FS.Collection("Images", {
-  stores: [new FS.Store.FileSystem("Images", {path: "~/uploads"})]
+    stores: [
+      new FS.Store.FileSystem("Images", {path: "~/uploads"}),
+      new FS.Store.FileSystem("thumbs", {path: "~/uploads",
+        transformWrite: function(fileObj, readStream, writeStream) {
+          // Transform the image into a 10x10px thumbnail
+          gm(readStream, fileObj.name()).resize('100', '100').stream().pipe(writeStream);
+        }
+      })
+    ]
+    // ,
+    // filter: {
+    //   allow: {
+    //     contentTypes: ['image/*'] //allow only images in this FS.Collection
+    //   }
+    // }
 });
-
-Meteor.methods({
-    addNews: function (title,url) {
-      alert("inside add_news");
-        // News.insert({
-        //     title: title,
-        //     url:url,
-        //     urlTitle:title.replace(/\s/g,'-'),
-        //     dateAdded:new Date()
-        // });
-    }
-});
-
 
 if (Meteor.isClient) {
 
@@ -29,16 +43,21 @@ if (Meteor.isClient) {
 
     Template.imageView.helpers({
       images: function () {
-        return Images.find(); // Where Images is an FS.Collection instance
+        var current_news = this._id;
+        imageUrl = Images.findOne({owner:current_news}, {}).url("thumbs");
+        var returnedImage = {url : imageUrl}; // Where Images is an FS.Collection instance
+        return returnedImage;
       }
     });
 
     Template.addNews.events({
        'submit .addNewsForm':function(e, template){
+           e.preventDefault();
            var title= e.target.title.value;
 
            var url= e.target.url.value;
-           alert("hello1")
+
+           var fileObj = template.find('input:file');
 
            var news_lat = News.insert({
             title: title,
@@ -48,47 +67,27 @@ if (Meteor.isClient) {
            }, function(err, result){
 
             if(result){
-              alert(result);
-              alert("news successfully added");
-
+              // alert(result);
+              // alert("news successfully added");
+              // var fileObj = template.find('input:file');
+              // alert("file object")
+              // alert(fileObj);
+              var imageObj = Images.insert(fileObj.files[0], function (err, fileObj) {
+              });
+              var imageUpdated = Images.update(
+              {_id:imageObj._id},
+              {
+                $set: {owner:result, owner_type:'news'},
+              }
+              );
+              console.log(imageUpdated);
+              // alert("image inserted successfully");
            }
          });
-           alert("hello2")
 
-            var files = template.find('input:file');
-            alert(files);
-            for (var i = 0, ln = files.length; i < ln; i++) {
-            alert("inside for loop");
-            Images.insert(files[i], function (err, fileObj) {
-              alert("image inserted successfully")
-              alert(fileObj);
-              // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-            });
-          }
-          alert("after for loop");
+         Router.go('news.all');
 
-          // FS.Utility.eachFile(event, function(file) {
-          //     alert("in utility");
-          //     alert(file);
-          //     Images.insert(file, function (err, fileObj) {
-          //       if (err){
-          //         // handle error
-          //       } else {
-          //           // handle success depending what you need to do
-          //         var userId = Meteor.userId();
-          //         var imagesURL = {
-          //           "profile.image": "/cfs/files/images/" + fileObj._id
-          //         };
-          //         News.update({_id:news_lat}, {$set: imagesURL});
-          //       }
-          //     });
-          //   });
-          alert("after utility");
-           // Meteor.call('addNews',title,url);
-
-           Router.go('news.all');
-
-           return false;
+         return false;
        }
     });
 }
